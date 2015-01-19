44ada5df172fbd1571fea807ca41f95400c6ca1c
Samy Pessé <samypesse@gmail.com>
2015-01-19 11:20:23 +0100

    Add parsing of langs and associated tests

---
+++ packages/@honkit/gitbook-markdown/test/langs.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+var langs = require('../').langs;
+
+var CONTENT = fs.readFileSync(path.join(__dirname, './fixtures/LANGS.md'), 'utf8');
+var LEXED = langs(CONTENT);
+
+describe('Languages parsing', function () {
+    it('should detect paths and titles', function() {
+        assert.equal(LEXED.list[0].path,'en');
+        assert.equal(LEXED.list[0].title,'English');
+
+        assert.equal(LEXED.list[1].path,'fr');
+        assert.equal(LEXED.list[1].title,'French');
+    });
+});
