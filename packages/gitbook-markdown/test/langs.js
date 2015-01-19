980ef23b47c1bad076d2589b84a6d7de5c689af3
Samy Pessé <samypesse@gmail.com>
2015-01-19 13:21:33 +0100

    Directly return list of langs

--- packages/gitbook-markdown/test/langs.js
+++ packages/gitbook-markdown/test/langs.js
@@ -9,12 +9,12 @@ var LEXED = langs(CONTENT);
 
 describe('Languages parsing', function () {
     it('should detect paths and titles', function() {
-        assert.equal(LEXED.list[0].path,'en/');
-        assert.equal(LEXED.list[0].lang,'en');
-        assert.equal(LEXED.list[0].title,'English');
+        assert.equal(LEXED[0].path,'en/');
+        assert.equal(LEXED[0].lang,'en');
+        assert.equal(LEXED[0].title,'English');
 
-        assert.equal(LEXED.list[1].path,'fr/');
-        assert.equal(LEXED.list[1].lang,'fr');
-        assert.equal(LEXED.list[1].title,'French');
+        assert.equal(LEXED[1].path,'fr/');
+        assert.equal(LEXED[1].lang,'fr');
+        assert.equal(LEXED[1].title,'French');
     });
 });
