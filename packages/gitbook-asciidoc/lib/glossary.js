60bc5281998e4ec9854e96ca6672a9551d1d0845
Samy Pessé <samypesse@gmail.com>
2015-01-21 11:02:50 +0100

    Complete parsing of glossary

--- packages/gitbook-asciidoc/lib/glossary.js
+++ packages/gitbook-asciidoc/lib/glossary.js
@@ -1,7 +1,34 @@
 var _ = require('lodash');
+var cheerio = require('cheerio');
+
+var convert = require('./utils/convert');
 
 function parseGlossary(src) {
-    return [];
+	var html = convert(src);
+    $ = cheerio.load(html);
+
+    var entries = [];
+
+    $("h2").each(function() {
+    	var $h2 = $(this);
+        var $p = $h2.next().find("p");
+
+    	var entry = {};
+
+    	entry.name = $h2.text();
+        entry.id = entryId(entry.name);
+        entry.description = $p.text();
+
+    	entries.push(entry);
+    });
+
+    return entries;
+}
+
+// Normalizes a glossary entry's name to create an ID
+function entryId(name) {
+    return name.toLowerCase();
 }
 
 module.exports = parseGlossary;
+module.exports.entryId = entryId;
