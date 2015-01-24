31313e3e8bc491b6ea0bbca66c8b81a298c81502
Samy Pessé <samypesse@gmail.com>
2015-01-24 23:21:09 +0100

    Delete test.js

--- packages/gitbook-asciidoc/test.js
+++
@@ -1,6 +0,0 @@
-var asciidoctor = require('asciidoctor.js')();
-var opal = asciidoctor.Opal;
-var processor = asciidoctor.Asciidoctor();
-var asciidocOpts = opal.hash2(['attributes'], {'attributes': 'showtitle'});
-
-console.log(processor.$convert('= Test', asciidocOpts));
