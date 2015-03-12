ca2d0ab172fe6f84c67e313748a966dcddebd2bc
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-03-12 19:33:49 +0100

    Add lib/annotate_engine.js
    Decouple rules and annotation execution

--- packages/gitbook-markdown/lib/annotate.js
+++ packages/gitbook-markdown/lib/annotate.js
@@ -1,3 +1,5 @@
+var engine = require('./annotate_engine');
+
 // Pulled from "kramed.Lexer.rules.tables"
 var rules = { newline: /^\n+/,
     code: /^( {4}[^\n]+\n*)+/,
@@ -36,45 +38,7 @@ var ruleMap = {
 }
 
 function annotate(src) {
-    var tokens = [];
-
-    while(src) {
-        // Pick rule
-        var rule = ruleTypes.filter(function(ruleName, idx) {
-            var regex = rules[ruleName];
-            return regex.exec(src);
-        })[0];
-
-        // No matching rules
-        if(!rule) {
-            throw new Error('No rule found for: ' + src);
-        }
-
-        // Use rule to extract block
-        var ruleRegex = rules[rule];
-        var block = ruleRegex.exec(src);
-
-        // Get rule type
-        var type = ruleMap[rule] || rule;
-
-        // Get raw text
-        var raw = block[0];
-
-        // Break out here to avoid infinite loops
-        if(raw.length === 0) {
-            break;
-        }
-
-        tokens.push({
-            type: ruleMap[rule] || rule,
-            raw: raw,
-        });
-
-        // Update source
-        src = src.substring(raw.length);
-    }
-
-    return tokens
+    return engine(src, rules, ruleTypes, ruleMap);
 }
 
 module.exports = annotate;
---
+++ packages/gitbook-markdown/lib/annotate_engine.js
@@ -0,0 +1,43 @@
+function annotateEngine(src, rules, ruleTypes, ruleMap) {
+    var tokens = [];
+
+    while(src) {
+        // Pick rule
+        var rule = ruleTypes.filter(function(ruleName, idx) {
+            var regex = rules[ruleName];
+            return regex.exec(src);
+        })[0];
+
+        // No matching rules
+        if(!rule) {
+            throw new Error('No rule found for: ' + src);
+        }
+
+        // Use rule to extract block
+        var ruleRegex = rules[rule];
+        var block = ruleRegex.exec(src);
+
+        // Get rule type
+        var type = ruleMap[rule] || rule;
+
+        // Get raw text
+        var raw = block[0];
+
+        // Break out here to avoid infinite loops
+        if(raw.length === 0) {
+            break;
+        }
+
+        tokens.push({
+            type: ruleMap[rule] || rule,
+            raw: raw,
+        });
+
+        // Update source
+        src = src.substring(raw.length);
+    }
+
+    return tokens
+}
+
+module.exports = annotateEngine;
