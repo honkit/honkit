ec71897fc0445aa182d7cdd82dd1f8e66911d99c
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-03-12 20:09:59 +0100

    Update tests to reflect annotator

--- packages/gitbook-markdown/test/page.js
+++ packages/gitbook-markdown/test/page.js
@@ -21,28 +21,28 @@ describe('Page parsing', function() {
     });
 
     it('should escape codeblocks in preparation (1)', function() {
-        assert.equal(page.prepare("Hello `world`"), "\nHello {% raw %}`world`{% endraw %}\n");
-        assert.equal(page.prepare("Hello `world test`"), "\nHello {% raw %}`world test`{% endraw %}\n");
-        assert.equal(page.prepare("Hello ```world test```"), "\nHello {% raw %}`world test`{% endraw %}\n");
-        assert.equal(page.prepare("Hello\n```js\nworld test\n```\n"), "\nHello\n{% raw %}\n```js\nworld test\n```\n{% endraw %}");
-        assert.equal(page.prepare("Hello\n```\ntest\n\tworld\n\ttest\n```"), "\nHello\n{% raw %}\n```\ntest\n    world\n    test\n```\n{% endraw %}");
+        assert.equal(page.prepare("Hello `world`"), "Hello {% raw %}`world`{% endraw %}");
+        assert.equal(page.prepare("Hello `world test`"), "Hello {% raw %}`world test`{% endraw %}");
+        assert.equal(page.prepare("Hello ```world test```"), "Hello {% raw %}```world test```{% endraw %}");
+        assert.equal(page.prepare("Hello\n```js\nworld test\n```\n"), "Hello\n{% raw %}```js\nworld test\n```\n{% endraw %}");
+        assert.equal(page.prepare("Hello\n```\ntest\n\tworld\n\ttest\n```"), "Hello\n{% raw %}```\ntest\n\tworld\n\ttest\n```{% endraw %}");
     });
 
     it('should escape codeblocks in preparation (2)', function() {
         assert.equal(
             page.prepare("Hello\n\n\n\tworld\n\thello\n\n\ntest"),
-            "\nHello\n{% raw %}\n```\nworld\nhello\n```\n{% endraw %}\ntest\n"
+            "Hello\n\n\n{% raw %}\tworld\n\thello\n\n\n{% endraw %}test"
         );
         assert.equal(
             page.prepare("Hello\n\n\n\tworld\n\thello\n\n\n"),
-            "\nHello\n{% raw %}\n```\nworld\nhello\n```\n{% endraw %}"
+            "Hello\n\n\n{% raw %}\tworld\n\thello\n\n\n{% endraw %}"
         );
     });
 
     it('should escape codeblocks with nunjucks tags', function() {
         assert.equal(
             page.prepare('Hello {{ "Bonjour" }} ```test```'),
-            '\nHello {{ "Bonjour" }} {% raw %}`test`{% endraw %}\n'
+            'Hello {{ "Bonjour" }} {% raw %}```test```{% endraw %}'
         );
     });
 });
