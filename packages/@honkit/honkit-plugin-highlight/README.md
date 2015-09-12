b4f71faa23a3cd4c1350d095132f18f5c646ad07
Samy Pesse <samypesse@gmail.com>
2015-09-12 19:22:36 +0200

    Base code

---
+++ packages/@honkit/honkit-plugin-highlight/README.md
@@ -0,0 +1,13 @@
+# Code highlighting in GitBook
+
+This plugin is the default GitBook plugin used to highlight code blocks.
+
+You can remove it using:
+
+```
+{
+    plugins: ["-highlight"]
+}
+```
+
+
---
+++ packages/@honkit/honkit-plugin-highlight/index.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var hljs = require('highlight.js');
+
+var MAP = {
+    'py': 'python',
+    'js': 'javascript',
+    'json': 'javascript',
+    'rb': 'ruby',
+    'csharp': 'cs',
+};
+
+function normalize(lang) {
+    if(!lang) { return null; }
+
+    var lower = lang.toLowerCase();
+    return MAP[lower] || lower;
+}
+
+function highlight(lang, code) {
+    if(!lang) return code;
+
+    // Normalize lang
+    lang = normalize(lang);
+
+    try {
+        return hljs.highlight(lang, code).value;
+    } catch(e) { }
+
+    return code;
+}
+
+
+module.exports = {
+    book: {
+        assets: './css',
+        css: [
+            'website.css'
+        ]
+    },
+    ebook: {
+        assets: './css',
+        css: [
+            'ebook.css'
+        ]
+    },
+    blocks: {
+        code: function(block) {
+            return highlight(block.kwargs.lang, block.content);
+        }
+    }
+};
