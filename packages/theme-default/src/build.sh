e127d380407a326addf9ed9f8a1d0990fb75dc50
Samy Pessé <samypesse@gmail.com>
2016-02-18 09:47:42 +0100

    Copy all assets in build script

--- packages/theme-default/src/build.sh
+++ packages/theme-default/src/build.sh
@@ -19,3 +19,11 @@ lessc src/less/pdf.less _assets/ebook/pdf.css
 lessc src/less/mobi.less _assets/ebook/mobi.css
 lessc src/less/epub.less _assets/ebook/epub.css
 
+# Copy fonts
+mkdir -p _assets/website/fonts
+cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
+
+# Copy icons
+mkdir -p _assets/website/images
+cp node_modules/gitbook-logos/output/favicon.ico _assets/website/images/
+cp node_modules/gitbook-logos/output/apple-touch-icon-152.png _assets/website/images/apple-touch-icon-precomposed-152.png
