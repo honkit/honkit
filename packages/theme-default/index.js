33444f8334a6cc7ff93825de5a45c2dadb0bddc2
Samy Pessé <samypesse@gmail.com>
2016-02-25 14:55:30 +0100

    Include custom stylesheets

--- packages/theme-default/index.js
+++ packages/theme-default/index.js
@@ -2,8 +2,7 @@
 module.exports = {
     hooks: {
         config: function(config) {
-            // Compatibility with GitBook v2
-            config.pluginsConfig['theme-default'].styles = config.styles || config.pluginsConfig['theme-default'].styles;
+            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
 
             return config;
         }
