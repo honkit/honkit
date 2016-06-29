f31c4ef8b5637b2ce2002a57ee67e549ad69b04c
Samy Pesse <samypesse@gmail.com>
2016-06-29 23:10:56 +0200

    Adapt annotating code blocks

--- packages/gitbook-markdown/lib/page.js
+++ packages/gitbook-markdown/lib/page.js
@@ -1,10 +1,10 @@
 var _ = require('lodash');
 var MarkupIt = require('markup-it');
-var gitbookSyntax = require('markup-it/syntaxes/gitbook');
+var gitbookSyntax = require('markup-it/syntaxes/markdown');
 
 var RAW_START = '{% raw %}';
 var RAW_END   = '{% endraw %}';
-var gitbook  = new MarkupIt(gitbookSyntax);
+var markdown  = new MarkupIt(gitbookSyntax);
 
 /**
  * Escape a code block's content using raw blocks
@@ -26,31 +26,40 @@ function escape(str) {
  */
 function preparePage(src) {
     var levelRaw = 0;
+    var content  = markdown.toContent(src, {
+        math:     true,
+        template: true
+    });
 
-    return gitbook.annotate(src, function(token) {
-        var tokenType = token.getType();
+    var textMarkdown = markdown.toText(content, {
+        annotate: function(state, raw, token) {
+            var tokenType = token.getType();
 
-        if (tokenType === MarkupIt.BLOCKS.TEMPLATE && tokenType === MarkupIt.STYLES.TEMPLATE) {
-            var expr = token.getData().get('type');
-            if (expr === 'raw') {
-                levelRaw = levelRaw + 1;
-            } else if (expr == 'endraw') {
-                levelRaw = 0;
+            if (tokenType === MarkupIt.ENTITIES.TEMPLATE) {
+                var type = token.getData().get('type');
+                var expr = token.getAsPlainText();
+
+                if (type === 'expr') {
+                    if (expr === 'raw') {
+                        levelRaw = levelRaw + 1;
+                    } else if (expr == 'endraw') {
+                        levelRaw = 0;
+                    }
+                }
             }
 
-            return true;
+            if (
+                (tokenType === MarkupIt.BLOCKS.CODE || tokenType === MarkupIt.STYLES.CODE)
+                && levelRaw === 0
+            ) {
+                return escape(raw);
+            }
+
+            return raw;
         }
-
-        if (token.isBlock() && )
-
-
-
-        if (tokenType !== MarkupIt.BLOCKS.CODE && tokenType !== MarkupIt.STYLES.CODE && levelRaw == 0) {
-            return;
-        }
-
-        return escape(token.getRaw());
     });
+
+    return textMarkdown;
 }
 
 module.exports = {
--- packages/gitbook-markdown/test/page.js
+++ packages/gitbook-markdown/test/page.js
@@ -4,7 +4,6 @@ var assert = require('assert');
 
 var page = require('../').page;
 
-
 describe('Page parsing', function() {
     var LEXED;
 
@@ -23,53 +22,53 @@ describe('Page parsing', function() {
     });
 
     it('should escape codeblocks in preparation (1)', function() {
-        assert.equal(page.prepare("Hello `world`"), "Hello {% raw %}`world`{% endraw %}");
-        assert.equal(page.prepare("Hello `world test`"), "Hello {% raw %}`world test`{% endraw %}");
-        assert.equal(page.prepare("Hello ```world test```"), "Hello {% raw %}```world test```{% endraw %}");
-        assert.equal(page.prepare("Hello\n```js\nworld test\n```\n"), "Hello\n{% raw %}```js\nworld test\n```\n{% endraw %}");
-        assert.equal(page.prepare("Hello\n```\ntest\n\tworld\n\ttest\n```"), "Hello\n{% raw %}```\ntest\n\tworld\n\ttest\n```{% endraw %}");
+        assert.equal(page.prepare("Hello `world`"), 'Hello {% raw %}`world`{% endraw %}\n\n');
+        assert.equal(page.prepare("Hello `world test`"), 'Hello {% raw %}`world test`{% endraw %}\n\n');
+        assert.equal(page.prepare("Hello ```world test```"), 'Hello {% raw %}`world test`{% endraw %}\n\n');
+        assert.equal(page.prepare("Hello\n```js\nworld test\n```\n"), 'Hello\n\n{% raw %}```js\nworld test\n```\n\n{% endraw %}');
+        assert.equal(page.prepare("Hello\n```\ntest\n\tworld\n\ttest\n```"), 'Hello\n\n{% raw %}```\ntest\n    world\n    test\n```\n\n{% endraw %}');
     });
 
     it('should escape codeblocks in preparation (2)', function() {
         assert.equal(
             page.prepare("Hello\n\n\n\tworld\n\thello\n\n\ntest"),
-            "Hello\n\n\n{% raw %}\tworld\n\thello\n\n\n{% endraw %}test"
+            'Hello\n\n{% raw %}```\nworld\nhello```\n\n{% endraw %}test\n\n'
         );
         assert.equal(
             page.prepare("Hello\n\n\n\tworld\n\thello\n\n\n"),
-            "Hello\n\n\n{% raw %}\tworld\n\thello\n\n\n{% endraw %}"
+            'Hello\n\n{% raw %}```\nworld\nhello```\n\n{% endraw %}'
         );
     });
 
     it('should escape codeblocks with nunjucks tags', function() {
         assert.equal(
             page.prepare('Hello {{ "Bonjour" }} ```test```'),
-            'Hello {{ "Bonjour" }} {% raw %}```test```{% endraw %}'
+            'Hello {{ "Bonjour" }} {% raw %}`test`{% endraw %}\n\n'
         );
     });
 
     it('should escape codeblocks with nunjucks tags in {% raw %} tags', function() {
         assert.equal(
             page.prepare('{% raw %}Hello {{ "Bonjour" }} ```test```{% endraw %}'),
-            '{% raw %}Hello {{ "Bonjour" }} ```test```{% endraw %}'
+            '{% raw %}Hello {{ "Bonjour" }} `test`{% endraw %}\n\n'
         );
         assert.equal(
             page.prepare('{% raw %}Hello {{ "Bonjour" }} {% raw %}{% endraw %}```test```'),
-            '{% raw %}Hello {{ "Bonjour" }} {% raw %}{% endraw %}{% raw %}```test```{% endraw %}'
+            '{% raw %}Hello {{ "Bonjour" }} {% raw %}{% endraw %}{% raw %}`test`{% endraw %}\n\n'
         );
         assert.equal(
             page.prepare('```{% raw %}Hello {{ "Bonjour" }} {% raw %}```'),
-            '{% raw %}```{% raw %}Hello {{ "Bonjour" }} {% raw %}```{% endraw %}'
+            '{% raw %}`{% raw %}Hello {{ "Bonjour" }} {% raw %}`{% endraw %}\n\n'
         );
 
         assert.equal(
             page.prepare('```\ntest\n```\n\n\n### Test'),
-            '{% raw %}```\ntest\n```\n{% endraw %}\n\n### Test'
+            '{% raw %}```\ntest\n```\n\n{% endraw %}### Test\n\n'
         );
     });
 
     it('should not process math', function() {
-        assert.equal(page.prepare("Hello $world$"), "Hello $world$");
-        assert.equal(page.prepare("Hello $$world$$"), "Hello $$world$$");
+        assert.equal(page.prepare("Hello $world$"), 'Hello $world$\n\n');
+        assert.equal(page.prepare("Hello $$world$$"), 'Hello $$world$$\n\n');
     });
 });
