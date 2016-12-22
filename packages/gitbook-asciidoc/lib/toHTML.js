26b094d4c5af7e30ef76438c3e10b7e1dcb904e0
Guillaume Grossetie <g.grossetie@gmail.com>
2016-12-22 11:42:03 +0100

    Upgrade to Asciidoctor.js 1.5.5-4 (#17)

--- packages/gitbook-asciidoc/lib/toHTML.js
+++ packages/gitbook-asciidoc/lib/toHTML.js
@@ -1,21 +1,13 @@
 var asciidoctor = require('asciidoctor.js')();
-var opal = asciidoctor.Opal;
-var processor = asciidoctor.Asciidoctor(true);
 
 // Render Asciidoc to HTML (block)
 function asciidocToHTML(content) {
-    var options = opal.hash2(['attributes'], {'attributes': 'showtitle'});
-
-    var html = processor.$convert(content, options);
-    return html;
+    return asciidoctor.convert(content, {'attributes': 'showtitle'});
 };
 
 // Render Asciidoc to HTML (inline)
 function asciidocToHTMLInline(content) {
-    var options = Opal.hash({doctype: 'inline', attributes: ['showtitle']});
-
-    var html = processor.$convert(content, options);
-    return html;
+    return asciidoctor.convert(content, {doctype: 'inline', attributes: 'showtitle'});
 };
 
 module.exports = {
