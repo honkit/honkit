16d6265bfc3a0b4d58adbaf1d26609a87bd89759
Zachary Waldowski <zach@waldowski.me>
2019-12-03 04:41:38 -0500

    Small shim to replace the need for gitbook-cli

--- packages/gitbook/bin/gitbook.js
+++ packages/gitbook/bin/gitbook.js
@@ -1,8 +1,39 @@
 #! /usr/bin/env node
-/* eslint-disable no-console */
 
-var color = require('bash-color');
+const Gitbook = require('../lib');
+const program = require('commander');
+const assert = require('assert');
 
-console.log(color.red('You need to install "gitbook-cli" to have access to the gitbook command anywhere on your system.'));
-console.log(color.red('If you\'ve installed this package globally, you need to uninstall it.'));
-console.log(color.red('>> Run "npm uninstall -g gitbook" then "npm install -g gitbook-cli"'));
+Gitbook.commands.forEach(spec => {
+    var subcommand = program
+        .command(spec.name)
+        .description(spec.description);
+
+    const options = spec.options || [];
+    options.forEach(spec => {
+        if (spec.values) {
+            const template = `--${spec.name} <${spec.name}>`;
+            const description = `${spec.description} (${spec.values.map(JSON.stringify).join(', ')})`;
+            subcommand = subcommand.option(template, description, (value, _dummyPrevious) => {
+                assert(spec.values.includes(value), `Invalid value ${value} for ${spec.name}`);
+                return value;
+            }, spec.defaults);
+        } else {
+            subcommand = subcommand.option(`--${spec.name}`, spec.description, spec.defaults);
+        }
+    });
+
+    subcommand = subcommand.action((...joinedArgs) => {
+        const args = joinedArgs.slice(0, -1);
+        const kwargs = joinedArgs.slice(-1)[0];
+        spec
+            .exec(args, kwargs)
+            .catch(err => {
+                console.log(err.message);
+                if (program.debug || process.env.DEBUG) console.log(err.stack || '');
+                process.exit(1);
+            });
+    });
+});
+
+program.parse(process.argv);
