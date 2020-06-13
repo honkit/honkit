be34835b28b6326fafc5d138527da5af17fb631f
github-action-benchmark <github@users.noreply.github.com>
2020-06-13 15:40:26 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for f3803559ad219c05375b9fb217bfefa8b8f5ddf4

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1592062826368,
+  "repoUrl": "https://github.com/azu/githon",
+  "entries": {
+    "Secretlint benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "committer": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "distinct": true,
+          "id": "f3803559ad219c05375b9fb217bfefa8b8f5ddf4",
+          "message": "Update README",
+          "timestamp": "2020-06-14T00:39:22+09:00",
+          "tree_id": "c79481e29cc4987dc5fb101c5504c4c3f4eaf758",
+          "url": "https://github.com/azu/githon/commit/f3803559ad219c05375b9fb217bfefa8b8f5ddf4"
+        },
+        "date": 1592062825914,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run githon build",
+            "value": 0.93,
+            "range": "±1.79%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
