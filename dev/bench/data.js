a30482360fccaebdd5630dfc2a77fea30cdaee8c
github-action-benchmark <github@users.noreply.github.com>
2020-06-13 15:53:37 +0000

    add Githon benchmark (benchmarkjs) benchmark result for d93ada5c3a6780108cf64e37a1532861f8865a2b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1592063261677,
+  "lastUpdate": 1592063617454,
   "repoUrl": "https://github.com/azu/githon",
   "entries": {
     "Githon benchmark": [
@@ -29,6 +29,37 @@ window.BENCHMARK_DATA = {
             "name": "run githon build",
             "value": 0.06,
             "range": "±7.99%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "committer": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "distinct": true,
+          "id": "d93ada5c3a6780108cf64e37a1532861f8865a2b",
+          "message": "refactor: remove install plugin",
+          "timestamp": "2020-06-14T00:49:56+09:00",
+          "tree_id": "a347b9fddc8b4a97ccb243aa3d4c48d5a46f6dd5",
+          "url": "https://github.com/azu/githon/commit/d93ada5c3a6780108cf64e37a1532861f8865a2b"
+        },
+        "date": 1592063616983,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run githon build",
+            "value": 0.06,
+            "range": "±4.16%",
             "unit": "ops/sec",
             "extra": "5 samples"
           }
