a78dffd6fbc4771b7f98c11042797b9acfb75311
github-action-benchmark <github@users.noreply.github.com>
2020-06-13 15:42:42 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for 06f5a0fea90569fcd40d05b0d7fd4a4e146031b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1592062826368,
+  "lastUpdate": 1592062962129,
   "repoUrl": "https://github.com/azu/githon",
   "entries": {
     "Secretlint benchmark": [
@@ -29,6 +29,37 @@ window.BENCHMARK_DATA = {
             "name": "run githon build",
             "value": 0.93,
             "range": "±1.79%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "committer": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "distinct": true,
+          "id": "06f5a0fea90569fcd40d05b0d7fd4a4e146031b5",
+          "message": "Update README",
+          "timestamp": "2020-06-14T00:41:30+09:00",
+          "tree_id": "b103a59987a862827958e412a8ae06f301400953",
+          "url": "https://github.com/azu/githon/commit/06f5a0fea90569fcd40d05b0d7fd4a4e146031b5"
+        },
+        "date": 1592062961108,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run githon build",
+            "value": 0.96,
+            "range": "±2.43%",
             "unit": "ops/sec",
             "extra": "7 samples"
           }
