f0eaa4fc44ad1ced26145226ffd6ec91190d9bee
azu <azuciao@gmail.com>
2020-06-14 00:33:13 +0900

    chore: fix lint

--- examples/benchmark/benchmark.js
+++ examples/benchmark/benchmark.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const { execFileSync } = require("child_process");
 const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite();
@@ -6,10 +5,10 @@ const githonBin = require.resolve(".bin/githon");
 suite
     .add("run githon build", () => {
         execFileSync(githonBin, ["buil"], {
-            cwd: __dirname
+            cwd: __dirname,
         });
     })
-    .on("cycle", event => {
+    .on("cycle", (event) => {
         console.log(String(event.target));
     })
     .run();
