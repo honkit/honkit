1d2f0de7046c8bb697047a66af2680c766f5de12
azu <azuciao@gmail.com>
2020-06-16 23:55:01 +0900

    chore(githon): remove locateRootFolder.js
    fix https://github.com/azu/githon/issues/17

--- packages/githon/lib/plugins/__tests__/locateRootFolder.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const locateRootFolder = require("../locateRootFolder");
-
-describe("locateRootFolder", () => {
-    test.skip("should correctly resolve the node_modules for gitbook", () => {
-        expect(locateRootFolder()).toBe(path.resolve(__dirname, "../../../"));
-    });
-});
