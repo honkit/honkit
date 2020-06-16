89d6ce157d5e6c70de689ab9daab6b62f72cb3a6
github-action-benchmark <github@users.noreply.github.com>
2020-06-16 23:41:26 +0000

    add GitHon benchmark (benchmarkjs) benchmark result for 008d66f29b5488a37b456a4ba6bc426c9ff23a94

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1592350612985,
+  "lastUpdate": 1592350886529,
   "repoUrl": "https://github.com/azu/githon",
   "entries": {
     "GitHon benchmark": [
@@ -1920,6 +1920,37 @@ window.BENCHMARK_DATA = {
             "name": "run githon build",
             "value": 0.1,
             "range": "±1.33%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "committer": {
+            "email": "azuciao@gmail.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "distinct": true,
+          "id": "008d66f29b5488a37b456a4ba6bc426c9ff23a94",
+          "message": "fix(githon): disable confirm on copyDir\n\nhttps://github.com/GitbookIO/gitbook/pull/1681\nhttps://github.com/GitbookIO/gitbook/pull/1783\nhttps://github.com/GitbookIO/gitbook/pull/2091",
+          "timestamp": "2020-06-17T08:38:39+09:00",
+          "tree_id": "d8d7de4f31be0781605e6256a8486983735824df",
+          "url": "https://github.com/azu/githon/commit/008d66f29b5488a37b456a4ba6bc426c9ff23a94"
+        },
+        "date": 1592350885932,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run githon build",
+            "value": 0.12,
+            "range": "±1.01%",
             "unit": "ops/sec",
             "extra": "5 samples"
           }
