d3850a9f21f7a01bf39b0ba94101629b69d376e8
azu <azuciao@gmail.com>
2020-06-20 09:06:04 +0900

    fix(githon): relax "gitbook" engine version check

--- packages/githon/lib/githon.js
+++ packages/githon/lib/githon.js
@@ -2,21 +2,26 @@ const semver = require("semver");
 const pkg = require("../package.json");
 
 const VERSION = pkg.version;
-const VERSION_STABLE = VERSION.replace(/\-(\S+)/g, "");
+const VERSION_STABLE = VERSION.replace(/-(\S+)/g, "");
 
 const START_TIME = new Date();
 
 /**
-    Verify that this gitbook version satisfies a requirement
-    We can't directly use samver.satisfies since it will break all plugins when gitbook version is a prerelease (beta, alpha)
+ Verify that this gitbook version satisfies a requirement
+ We can't directly use samver.satisfies since it will break all plugins when gitbook version is a prerelease (beta, alpha)
 
-    @param {String} condition
-    @return {Boolean}
-*/
+ @param {String} condition
+ @return {Boolean}
+ */
 function satisfies(condition) {
     // Test with real version
     if (semver.satisfies(VERSION, condition)) return true;
 
+    // If plugin require -alpha -beta engine, skip it
+    // https://github.com/githon/githon/issues/42
+    if (/-(\S+)$/.test(condition)) {
+        return true;
+    }
     // Test with future stable release
     return semver.satisfies(VERSION_STABLE, condition);
 }
