fdb496f1a117614b33990708f0c11575bc9fe301
mizchi <miz404@gmail.com>
2020-06-21 20:56:36 +0900

    Fix roots

--- packages/githon/jest.config.js
+++ packages/githon/jest.config.js
@@ -1,4 +1,6 @@
+const path = require("path");
 module.exports = {
+    roots: [path.join(__dirname, "src")],
     preset: "ts-jest",
     setupFilesAfterEnv: ["./testing/setup.js"],
     testEnvironment: "node",
