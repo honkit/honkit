0e42eb34a025a2c3ffcdb2bc2de441fdfaa8246a
masnagam <masnagam@gmail.com>
2020-11-27 17:57:04 +0900

    style(honkit-plugin-highlight): `yarn run lint` and `yarn prettier`

--- packages/@honkit/honkit-plugin-highlight/index.js
+++ packages/@honkit/honkit-plugin-highlight/index.js
@@ -1,57 +1,54 @@
-var path = require('path');
-var hljs = require('highlight.js');
+const hljs = require("highlight.js");
 
-var MAP = {
-    'py': 'python',
-    'js': 'javascript',
-    'json': 'javascript',
-    'rb': 'ruby',
-    'csharp': 'cs',
+const MAP = {
+    py: "python",
+    js: "javascript",
+    json: "javascript",
+    rb: "ruby",
+    csharp: "cs",
 };
 
 function normalize(lang) {
-    if(!lang) { return null; }
+    if (!lang) {
+        return null;
+    }
 
-    var lower = lang.toLowerCase();
+    const lower = lang.toLowerCase();
     return MAP[lower] || lower;
 }
 
 function highlight(lang, code) {
-    if(!lang) return {
-        body: code,
-        html: false
-    };
+    if (!lang)
+        return {
+            body: code,
+            html: false,
+        };
 
     // Normalize lang
     lang = normalize(lang);
 
     try {
         return hljs.highlight(lang, code).value;
-    } catch(e) { }
+    } catch (e) {}
 
     return {
         body: code,
-        html: false
+        html: false,
     };
 }
 
-
 module.exports = {
     book: {
-        assets: './css',
-        css: [
-            'website.css'
-        ]
+        assets: "./css",
+        css: ["website.css"],
     },
     ebook: {
-        assets: './css',
-        css: [
-            'ebook.css'
-        ]
+        assets: "./css",
+        css: ["ebook.css"],
     },
     blocks: {
-        code: function(block) {
+        code: function (block) {
             return highlight(block.kwargs.language, block.body);
-        }
-    }
+        },
+    },
 };
