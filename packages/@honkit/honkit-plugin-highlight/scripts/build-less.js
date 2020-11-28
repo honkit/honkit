41dac5a4af84a34ca33e5a792d60eea0abc712d2
masnagam <masnagam@gmail.com>
2020-11-28 10:08:31 +0900

    fix(honkit-plugin-highlight): fix issues regarding coding styles

--- packages/@honkit/honkit-plugin-highlight/scripts/build-less.js
+++ packages/@honkit/honkit-plugin-highlight/scripts/build-less.js
@@ -1,37 +1,49 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const highlightJs = require('highlight.js');
+const highlightJs = require("highlight.js");
 
-const SOLARIZED_LIGHT_CSS = readCss('solarized-light.css');
-const TOMORROW_CSS = readCss('tomorrow.css');
-const TOMORROW_NIGHT_BRIGHT_CSS = readCss('tomorrow-night-bright.css');
+const SOLARIZED_LIGHT_CSS = readCss("solarized-light.css");
+const TOMORROW_CSS = readCss("tomorrow.css");
+const TOMORROW_NIGHT_BRIGHT_CSS = readCss("tomorrow-night-bright.css");
 
-writeLess('ebook.less', `
+writeLess(
+    "ebook.less",
+    `
 pre, code {
 /* From highlight.js@${highlightJs.versionString}/styles/tomorrow.css */
 
 ${TOMORROW_CSS}
 }
-`);
+`
+);
 
-writeLess('night.less', `
+writeLess(
+    "night.less",
+    `
 /* From highlight.js@${highlightJs.versionString}/styles/tomorrow-night-bright.css */
 
 ${TOMORROW_NIGHT_BRIGHT_CSS}
-`);
+`
+);
 
-writeLess('sepia.less', `
+writeLess(
+    "sepia.less",
+    `
 /* From highlight.js@${highlightJs.versionString}/styles/solarized-light.css */
 
 ${SOLARIZED_LIGHT_CSS}
-`);
+`
+);
 
-writeLess('white.less', `
+writeLess(
+    "white.less",
+    `
 /* From highlight.js@${highlightJs.versionString}/styles/tomorrow.css */
 
 ${TOMORROW_CSS}
-`);
+`
+);
 
 function readCss(filename) {
     // We assume the directory structure of the highlight.js module like below:
@@ -41,11 +53,11 @@ function readCss(filename) {
     //           +-- lib/index.js
     //           +-- styles/*.css
     //
-    const STYLES_DIR = path.resolve(require.resolve('highlight.js'), '..', '..', 'styles');
-    return fs.readFileSync(path.join(STYLES_DIR, filename), 'utf8');
+    const STYLES_DIR = path.resolve(require.resolve("highlight.js"), "..", "..", "styles");
+    return fs.readFileSync(path.join(STYLES_DIR, filename), "utf8");
 }
 
 function writeLess(filename, str) {
-    const LESS_DIR = path.resolve(__dirname, '..', 'less');
-    fs.writeFileSync(path.join(LESS_DIR, filename), str.trim() + '\n', 'utf8');
+    const LESS_DIR = path.resolve(__dirname, "..", "less");
+    fs.writeFileSync(path.join(LESS_DIR, filename), `${str.trim()}\n`, "utf8");
 }
