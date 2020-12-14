6335bc9e020b2668d0fe4989c7f9e60899b9dcbc
Guillaume Grossetie <ggrossetie@gmail.com>
2020-12-14 09:34:46 +0100

    feat(asciidoc) use font icons by default

--- packages/@honkit/asciidoc/src/toHTML.ts
+++ packages/@honkit/asciidoc/src/toHTML.ts
@@ -4,12 +4,12 @@ const asciidoctor = Asciidoctor();
 
 // Render Asciidoc to HTML (block)
 function asciidocToHTML(content: string) {
-    return asciidoctor.convert(content, { safe: "server", attributes: "showtitle" });
+    return asciidoctor.convert(content, { safe: "server", attributes: { showtitle: "", icons: "font@" } });
 }
 
 // Render Asciidoc to HTML (inline)
 function asciidocToHTMLInline(content: string) {
-    return asciidoctor.convert(content, { doctype: "inline", attributes: "showtitle" });
+    return asciidoctor.convert(content, { doctype: "inline" });
 }
 
 export default {
