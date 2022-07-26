282788c3a8d6adb6d36b3a3c694ce5fb1c554439
Andrew Berry <andrew@furrypaws.ca>
2022-07-25 21:08:17 -0400

    Add a unit test showing glossary order bug

--- packages/honkit/src/output/modifiers/__tests__/annotateText.ts
+++ packages/honkit/src/output/modifiers/__tests__/annotateText.ts
@@ -28,6 +28,25 @@ describe("annotateText", () => {
         expect(words.hasClass("glossary-term")).toBeTruthy();
     });
 
+    test("should annotate text with regardless of glossary order", () => {
+        const $ = cheerio.load("<p>This is multiple words, and another word.</p>", { _useHtmlParser2: true });
+
+        annotateText(entries, "GLOSSARY.md", $);
+
+        const links = $("a");
+        expect(links.length).toBe(2);
+
+        const word = $(links.get(0));
+        expect(word.attr("href")).toBe("/GLOSSARY.md#multiple-words");
+        expect(word.text()).toBe("multiple words");
+        expect(word.hasClass("glossary-term")).toBeTruthy();
+
+        const words = $(links.get(1));
+        expect(words.attr("href")).toBe("/GLOSSARY.md#word");
+        expect(words.text()).toBe("word");
+        expect(words.hasClass("glossary-term")).toBeTruthy();
+    });
+
     test("should not annotate scripts", () => {
         const $ = cheerio.load("<script>This is a word, and multiple words</script>", { _useHtmlParser2: true });
 
