51addf7df94bbcd3731948b0bb9a7e998aecf3eb
azu <azuciao@gmail.com>
2022-09-16 21:52:03 +0900

    test: fix timeout

--- packages/honkit/src/__tests__/snapshot-honkit.ts
+++ packages/honkit/src/__tests__/snapshot-honkit.ts
@@ -3,7 +3,6 @@ import { iterateDirectoryContents } from "@honkit/internal-test-utils";
 import * as bin from "../bin";
 
 it("HonKit snapshots", async () => {
-    jest.setTimeout(60 * 1000);
     const bookDir = path.join(__dirname, "__fixtures__/honkit");
     const outputDir = path.join(__dirname, "__fixtures__/honkit/_book");
     await bin.run([process.argv[0], ".", "build", bookDir, "--reload"]);
@@ -19,4 +18,4 @@ it("HonKit snapshots", async () => {
     })) {
         expect(item).toMatchSnapshot(item.filePath);
     }
-});
+}, 60 * 1000);
