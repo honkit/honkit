df0309163058a30df5209e7b8423267db77f19f1
azu <azuciao@gmail.com>
2023-07-31 21:26:15 +0900

    add `window.honkit.metadata`

--- packages/@honkit/theme/index.js
+++ packages/@honkit/theme/index.js
@@ -2,7 +2,6 @@ module.exports = {
     hooks: {
         config: function (config) {
             config.styles = config.styles || config.pluginsConfig["@honkit/honkit-plugin-theme"].styles;
-
             return config;
         }
     }
