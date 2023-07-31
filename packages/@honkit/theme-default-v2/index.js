f7957f7280d6280688439415751949646fafb43e
azu <azuciao@gmail.com>
2023-07-31 21:04:51 +0900

    v1

--- packages/@honkit/theme-default-v2/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    hooks: {
-        config: function (config) {
-            config.styles = config.styles || config.pluginsConfig["@honkit/honkit-plugin-theme-default-v2"].styles;
-
-            return config;
-        }
-    }
-};
---
+++ packages/@honkit/theme/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    hooks: {
+        config: function (config) {
+            config.styles = config.styles || config.pluginsConfig["@honkit/honkit-plugin-theme"].styles;
+
+            return config;
+        }
+    }
+};
