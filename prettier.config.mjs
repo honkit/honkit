2c09f5faffbb336b6a1761f04adfc32a897b875a
David Millar <dave@davegoesthedistance.com>
2024-10-15 20:19:42 -0500

    Lint format simplify (#443)
    * refactor(linting): centralized eslint configs
    
    Created a new package that contains a shared config for eslint.
    
    Inspired by https://medium.com/reactbrasil/reuse-your-eslint-prettier-config-in-a-monorepo-with-lerna-54c1800cacdc
    but had trouble centralizing the Prettier config without making a second package.
    
    * chore(lint): updated prettier config
    
    Moved configs to editorconfig where appropriate to reduce chance of
    conflicting or duplicate configs. Moved remaining prettier config to
    flat file. Previous eslint cleanup also removed things that Prettier
    and editorconfig define and enforce.
    
    * feat(dev): add clean-deps script
    
    Convenience method for clearing node modules.
    
    * chore(deps): make eslint a peerDep of lint tools

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+// prettier.config.mjs
+// Prettier also enforces some conditions in .editorconfig.
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    singleQuote: false,
+    trailingComma: "none"
+};
+
+export default config;
